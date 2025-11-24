d46c5d24d13bab5f95aa296741b83c53b59dcc57
Brian Bushnell <bbushnell@lbl.gov>
2025-11-23 19:32:55 -0800

    Added scarf, gfa, fastg support to FastqScan

--- fastqscan.sh
+++ fastqscan.sh
@@ -3,7 +3,7 @@
 usage(){
 echo "
 Written by Brian Bushnell
-Last modified November 22, 2025
+Last modified November 23, 2025
 
 Description:  Fast lightweight scanner that parses sequence files.
 Reports base and record counts.  Performs basic integrity checks;
@@ -12,8 +12,9 @@ Does not perform rigorous validation of all fields.
 
 Usage:  fastqscan.sh <file>
 
-Input may be fastq, fasta, or sam, compressed or uncompressed.
-To input stdin use e.g. stdin.fq as the argument (with proper extension).
+Input may be fastq, fasta, sam, scarf, gfa, or fastg, 
+compressed or uncompressed.  To input stdin use e.g. stdin.fq
+as the argument (with proper extension).
 
 Please contact Brian Bushnell at bbushnell@lbl.gov if you encounter any problems.
 For documentation and the latest version, visit: https://bbmap.org
@@ -38,8 +39,8 @@ resolveSymlinks(){
 
 EA="-ea"
 SIMD="--add-modules jdk.incubator.vector"
-XMX="-Xmx256m"
-XMS="-Xms256m"
+XMX="-Xmx1g"
+XMS="-Xms128m"
 
 setEnv(){
 	. "$DIR/javasetup.sh"
