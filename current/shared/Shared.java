0195a525e06cd27533e208bcda0c6df55d481c14
Brian Bushnell <bbushnell@lbl.gov>
2026-03-28 11:00:39 -0700

    v39.81 with DynamicLogLog!

--- current/shared/Shared.java
+++ current/shared/Shared.java
@@ -37,9 +37,9 @@ public class Shared {
 
 	// Version and identification
 	/** Version String, proper float with in XX.xx format */
-	public static String BBTOOLS_VERSION_STRING="39.80";
+	public static String BBTOOLS_VERSION_STRING="39.81";
 	/** Release name */
-	public static String BBMAP_VERSION_NAME="Cardinality Concerto";
+	public static String BBMAP_VERSION_NAME="DynamicLogLog Advent";
 	/** Main class name for current execution */
 	public static String BBMAP_CLASS=null;
 	/** Class object for main executing class */
