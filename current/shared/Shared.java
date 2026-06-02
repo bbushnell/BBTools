f0efe7270db9e02473df71a40a0c44db145ee8bd
Brian Bushnell <bbushnell@lbl.gov>
2026-06-01 21:10:14 -0700

    New version 39.89

--- current/shared/Shared.java
+++ current/shared/Shared.java
@@ -37,9 +37,9 @@ public class Shared {
 
 	// Version and identification
 	/** Version String, proper float with in XX.xx format */
-	public static String BBTOOLS_VERSION_STRING="39.88";
+	public static String BBTOOLS_VERSION_STRING="39.89";
 	/** Release name */
-	public static String BBMAP_VERSION_NAME="Black Leather Boots";
+	public static String BBMAP_VERSION_NAME="DDL Sketch";
 	/** Main class name for current execution */
 	public static String BBMAP_CLASS=null;
 	/** Class object for main executing class */
