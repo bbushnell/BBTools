38bd726ea4400b3ff7e2e19fd4271bdc612ca6b3
Brian Bushnell <bbushnell@lbl.gov>
2026-07-05 22:55:30 -0700

    New version, 39.94

--- current/shared/Shared.java
+++ current/shared/Shared.java
@@ -37,9 +37,9 @@ public class Shared {
 
 	// Version and identification
 	/** Version String, proper float with in XX.xx format */
-	public static String BBTOOLS_VERSION_STRING="39.93";
+	public static String BBTOOLS_VERSION_STRING="39.94";
 	/** Release name */
-	public static String BBMAP_VERSION_NAME="Strong Sturgeon";
+	public static String BBMAP_VERSION_NAME="Great Bughunt";
 	/** Main class name for current execution */
 	public static String BBMAP_CLASS=null;
 	/** Class object for main executing class */
